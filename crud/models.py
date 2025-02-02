827fcc001b333c55682bf4b31213c6ba372e58e4
Danylo Krukovskyi <ipz233_kds@ztu.edu.ua>
2025-02-02 21:59:15 +0200

    API

---
+++ crud/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./test.db"
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
